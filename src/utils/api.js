6bf9adaaa194ab678d7863def15717aaa5b9dad3
yyeojung <hongshii0i0i@gmail.com>
2025-08-05 21:44:42 +0900

    console 주석 처리

--- src/utils/api.js
+++ src/utils/api.js
@@ -16,7 +16,7 @@ const api = axios.create({
  */
 api.interceptors.request.use(
   (request) => {
-    console.log("Starting Request", request);
+    // console.log("Starting Request", request);
     request.headers.authorization = `Bearer ${sessionStorage.getItem("token")}`;
     return request;
   },
