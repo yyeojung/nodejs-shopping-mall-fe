ecd43d8ae96a2ccbaf51114f441d49a7ac36506a
yyeojung <hongshii0i0i@gmail.com>
2025-08-06 22:35:05 +0900

    페이지네이션 적용 (SearchBox가 아니어서 useEffect로 searchQuery 상태 업데이트)

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -1,24 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./components/ProductCard";
 import { Row, Col, Container } from "react-bootstrap";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductList } from "../../features/product/productSlice";
+import { DotLoader } from "react-spinners";
+import ReactPaginate from "react-paginate";
 
 const LandingPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const productList = useSelector((state) => state.product.productList);
+  const { productList, totalPageNum, isLoading } = useSelector(
+    (state) => state.product
+  );
   const [query] = useSearchParams();
-  const name = query.get("name");
-  useEffect(() => {
-    dispatch(
-      getProductList({
-        name,
-      })
-    );
-  }, [query]);
+  const [searchQuery, setSearchQuery] = useState({
+    pageSize: 8,
+    page: query.get("page") || 1,
+    name: query.get("name") || "",
+  }); //검색 조건들을 저장하는 객체
 
+  useEffect(() => {
+    dispatch(getProductList({ ...searchQuery }));
+  }, [query, searchQuery, dispatch]);
+
+  useEffect(() => {
+    setSearchQuery({
+      pageSize: 8,
+      page: query.get("page") || 1,
+      name: query.get("name") || "",
+    });
+  }, [query]);
+  useEffect(() => {
+    //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if (searchQuery.name === "") {
+      delete searchQuery.name;
+    }
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    navigate(`?${query}`);
+  }, [searchQuery, navigate]);
+
+  if (isLoading) {
+    return (
+      <div className="loading-container">
+        <DotLoader color="#C81E2B" />
+      </div>
+    );
+  }
+  const handlePageClick = ({ selected }) => {
+    //  쿼리에 페이지값 바꿔주기
+    setSearchQuery({ ...searchQuery, page: selected + 1 });
+  };
   return (
     <Container>
       <Row>
@@ -30,13 +64,34 @@ const LandingPage = () => {
           ))
         ) : (
           <div className="text-align-center empty-bag">
-            {name === "" ? (
+            {searchQuery.name === "" ? (
               <h2>등록된 상품이 없습니다!</h2>
             ) : (
-              <h2>{name}과 일치한 상품이 없습니다!`</h2>
+              <h2>{searchQuery.name}과 일치한 상품이 없습니다!</h2>
             )}
           </div>
         )}
+        <ReactPaginate
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={totalPageNum}
+          forcePage={searchQuery.page - 1}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+          className="display-center list-style-none"
+        />
       </Row>
     </Container>
   );
