a47b03170205fd3232ae90767dab23792e4c0aae
yyeojung <hongshii0i0i@gmail.com>
2025-08-06 22:34:22 +0900

    페이지네이션 적용

--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -38,11 +38,17 @@ const AdminProductPage = () => {
 
   //상품리스트 가져오기 (url쿼리 맞춰서)
   useEffect(() => {
-    dispatch(getProductList());
-  }, []);
+    dispatch(getProductList({ ...searchQuery }));
+  }, [query]);
 
   useEffect(() => {
     //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if (searchQuery.name === "") {
+      delete searchQuery.name;
+    }
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    navigate(`?${query}`);
   }, [searchQuery]);
 
   const deleteItem = (id) => {
@@ -63,8 +69,12 @@ const AdminProductPage = () => {
 
   const handlePageClick = ({ selected }) => {
     //  쿼리에 페이지값 바꿔주기
+    setSearchQuery({ ...searchQuery, page: selected + 1 });
   };
 
+  // searchbox에서 검색어를 읽어온다 => 엔터를 치면 searchQuery객체가 업데이트 된다. { name: 스트레이트 팬츠}
+  // searchQuery 객체 안에 아이템 기준으로 url을 새로 생성해서 호출 &name=스트레이트+팬츠
+  // url쿼리 읽어오기 => url 쿼리 기준으로 be에 검색조건과 함꼐 호출
   return (
     <div className="locate-center">
       <Container>
@@ -90,7 +100,7 @@ const AdminProductPage = () => {
           nextLabel="next >"
           onPageChange={handlePageClick}
           pageRangeDisplayed={5}
-          pageCount={100}
+          pageCount={totalPageNum}
           forcePage={searchQuery.page - 1}
           previousLabel="< previous"
           renderOnZeroPageCount={null}
