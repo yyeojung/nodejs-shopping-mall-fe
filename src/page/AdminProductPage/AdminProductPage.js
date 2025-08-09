2eb5a0c6eb1a73fdd48ae4edb814cabf2ea20cff
yyeojung <hongshii0i0i@gmail.com>
2025-08-09 14:19:26 +0900

    상품 수정 모달 오픈

--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -57,7 +57,10 @@ const AdminProductPage = () => {
 
   const openEditForm = (product) => {
     //edit모드로 설정하고
+    setMode("edit");
     // 아이템 수정다이얼로그 열어주기
+    dispatch(setSelectedProduct(product));
+    setShowDialog(true);
   };
 
   const handleClickNewItem = () => {
