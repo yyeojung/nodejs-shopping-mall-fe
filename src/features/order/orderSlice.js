128ae873015d1348600f40c7cf216f4d1f5ce461
yyeojung <hongshii0i0i@gmail.com>
2025-08-10 16:32:21 +0900

    createOrder

--- src/features/order/orderSlice.js
+++ src/features/order/orderSlice.js
@@ -16,7 +16,16 @@ const initialState = {
 // Async thunks
 export const createOrder = createAsyncThunk(
   "order/createOrder",
-  async (payload, { dispatch, rejectWithValue }) => {}
+  async (payload, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.post("/order", payload);
+      if (response.status !== 200) throw new Error(response.error);
+      return response.data.orderNum;
+    } catch (error) {
+      dispatch(showToastMessage({ message: error.error, status: "error" }));
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const getOrder = createAsyncThunk(
@@ -43,7 +52,21 @@ const orderSlice = createSlice({
       state.selectedOrder = action.payload;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createOrder.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(createOrder.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = "";
+        state.selectedOrder = action.payload;
+      })
+      .addCase(createOrder.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const { setSelectedOrder } = orderSlice.actions;
