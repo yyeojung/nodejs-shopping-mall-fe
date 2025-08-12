4559193342d941c74f561f832b1b7cd13b4497b5
yyeojung <hongshii0i0i@gmail.com>
2025-08-12 22:34:46 +0900

    map key값 추가

--- src/page/AdminOrderPage/component/OrderTable.js
+++ src/page/AdminOrderPage/component/OrderTable.js
@@ -9,16 +9,16 @@ const OrderTable = ({ header, data, openEditForm }) => {
       <Table striped bordered hover>
         <thead>
           <tr>
-            {header.map((title) => (
-              <th>{title}</th>
+            {header.map((title, index) => (
+              <th key={index}>{title}</th>
             ))}
           </tr>
         </thead>
         <tbody>
           {data.length > 0 ? (
             data.map((item, index) => (
-              <tr onClick={() => openEditForm(item)}>
-                <th>{index}</th>
+              <tr key={index} onClick={() => openEditForm(item)}>
+                <th>{index + 1}</th>
                 <th>{item.orderNum}</th>
                 <th>{item.createdAt.slice(0, 10)}</th>
                 <th>{item.userId.email}</th>
