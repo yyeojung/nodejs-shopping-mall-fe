633227bcc775750a093c4ee5845d5bfaec9ba4f9
yyeojung <hongshii0i0i@gmail.com>
2025-08-17 00:15:26 +0900

    loginWithGoogle 추가

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,7 +2,6 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import { showToastMessage } from "../common/uiSlice";
 import api from "../../utils/api";
-import { initialCart } from "../cart/cartSlice";
 
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
@@ -22,7 +21,16 @@ export const loginWithEmail = createAsyncThunk(
 
 export const loginWithGoogle = createAsyncThunk(
   "user/loginWithGoogle",
-  async (token, { rejectWithValue }) => {}
+  async (token, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/users/google", { token });
+      sessionStorage.setItem("token", response.data.token);
+
+      return response.data.user;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
 // export const logout = () => (dispatch) => {
@@ -118,6 +126,18 @@ const userSlice = createSlice({
       })
       .addCase(loginWithToken.fulfilled, (state, action) => {
         state.user = action.payload.user;
+      })
+      .addCase(loginWithGoogle.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginWithGoogle.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        state.loginError = null;
+      })
+      .addCase(loginWithGoogle.rejected, (state, action) => {
+        state.loading = false;
+        state.loginError = action.payload;
       });
   },
 });
